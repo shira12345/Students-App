7faa78c0fd874be5709d771a3f10a418181e8b40
Natan Sinai <natanbriman@gmail.com>
2026-01-04 18:56:09 +0200

    feat: adding edit student screen and save & cancel buttons. (#3)
    * feat: adding edit student screen and save & cancel buttons.
    
    * fix: remove force unwrapping and replace

--- app/src/main/java/com/example/studentsapp/EditStudentActivity.kt
+++ app/src/main/java/com/example/studentsapp/EditStudentActivity.kt
@@ -1,5 +1,6 @@
 package com.example.studentsapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -9,7 +10,13 @@ import com.example.studentsapp.databinding.ActivityEditStudentBinding
 
 class EditStudentActivity : AppCompatActivity() {
   private lateinit var binding: ActivityEditStudentBinding
-  private lateinit var student: Student
+  private var student: Student? = null
+  private var position: Int = -1
+
+  companion object {
+    const val EXTRA_STUDENT = "extra_student"
+    const val EXTRA_POSITION = "extra_position"
+  }
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -23,5 +30,55 @@ class EditStudentActivity : AppCompatActivity() {
       v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
       insets
     }
+
+    @Suppress("DEPRECATION")
+    student = intent.getParcelableExtra(EXTRA_STUDENT)
+    position = intent.getIntExtra(EXTRA_POSITION, -1)
+
+    fillInputs()
+
+    binding.saveStudentButton.setOnClickListener {
+      updateStudent()
+
+      returnToStudentsListWithUpdatedStudent()
+    }
+
+    binding.cancelButton.setOnClickListener {
+      setResult(RESULT_CANCELED)
+
+      finish()
+    }
+  }
+
+  private fun fillInputs() {
+    student?.let { (id, name, phone, address, isChecked) ->
+      binding.apply {
+        editStudentName.setText(name)
+        editStudentId.setText(id)
+        editStudentPhone.setText(phone)
+        editStudentAddress.setText(address)
+        editStudentCheckbox.isChecked = isChecked
+      }
+    }
+  }
+
+  private fun updateStudent() {
+    student?.apply {
+      name = binding.editStudentName.text.toString()
+      id = binding.editStudentId.text.toString()
+      phone = binding.editStudentPhone.text.toString()
+      address = binding.editStudentAddress.text.toString()
+      isChecked = binding.editStudentCheckbox.isChecked
+    }
+  }
+
+  private fun returnToStudentsListWithUpdatedStudent() {
+    val resultIntent = Intent().apply {
+      putExtra(EXTRA_STUDENT, student)
+      putExtra(EXTRA_POSITION, position)
+    }
+
+    setResult(RESULT_OK, resultIntent)
+    finish()
   }
 }
